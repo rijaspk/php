617dd2d517d8a237e3eeb768321480973a41ec51
“Rijas <Rijas.Ahammed@icfoss.in>
2018-04-09 16:15:56 +0530

    first commit

---
+++ styles.css.php
@@ -0,0 +1,17 @@
+<?php
+header("Content-type:text/css");
+
+?>
+
+
+
+.email{
+ font-weight: bold; 
+ <?php echo $disableRemoveParam; ?>;
+color: <?php echo $linkColor; ?>;
+
+}
+.testclass{
+color:red;
+}
+
---
+++ test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="styles.css.php">
+<?php
+
+ $disableRemoveParam = "testclass";
+?>
+
+<style>
+.error {color: #FF0000;}
+
+</style>
+</head>
+<body> 
+
+<input type="button" value="email" class='" . $disableRemoveParam . "' />
+<p class='email'>asdasda</p>
+
+
+</body>
+</html>
